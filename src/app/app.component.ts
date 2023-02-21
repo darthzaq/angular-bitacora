f79362766b931671c11e642f32a671cdd5c986b2
Zaquiel Rodriguez Arce <darthzaq@gmail.com>
2023-02-21 15:00:00 +0100

    feat: todo screen moved to another component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,4 @@
 import { Component } from '@angular/core';
-import { Task } from './task/task'
-import { CdkDragDrop, transferArrayItem } from '@angular/cdk/drag-drop';
-import { MatDialog } from '@angular/material/dialog';
-import { TaskDialogComponent, TaskDialogResult } from './task-dialog/task-dialog.component';
-import { AngularFirestore } from '@angular/fire/compat/firestore';
-import { Observable, take } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -12,84 +6,5 @@ import { Observable, take } from 'rxjs';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  constructor(private dialog: MatDialog, private store: AngularFirestore) {}
-
-  todo = this.store.collection('todo').valueChanges({ idField: 'id' }) as Observable<Task[]>;
-  inProgress = this.store.collection('inProgress').valueChanges({ idField: 'id' }) as Observable<Task[]>;
-  done = this.store.collection('done').valueChanges({ idField: 'id' }) as Observable<Task[]>;
-
-  editTask(list: 'done' | 'todo' | 'inProgress', task: Task): void {
-    const dialogRef = this.dialog.open(TaskDialogComponent, {
-      width: '270px',
-      data: {
-        task,
-        enableDelete: true,
-      },
-    });
-    dialogRef.afterClosed().subscribe((result: TaskDialogResult|undefined) => {
-      if (!result) {
-        return;
-      }
-      if (result.delete) {
-        this.store.collection(list).doc(task.id).delete();
-      } else {
-        this.store.collection(list).doc(task.id).update(task);
-      }
-    });
-  }
-
-  drop(event: CdkDragDrop<Observable<Task[]>>): void {
-    if (event.previousContainer === event.container) {
-      return;
-    }
-
-    let previousContainer: Task[];
-    let container: Task[];
-
-    const updateData = () => {
-      if (previousContainer && previousContainer.length && container) {
-        const prevItem = previousContainer[event.previousIndex];
-
-        this.store.firestore.runTransaction(() => {
-          return Promise.all([
-            this.store.collection(event.previousContainer.id).doc(prevItem.id).delete(),
-            this.store.collection(event.container.id).add(prevItem),
-          ]);
-        });
-        transferArrayItem(
-          previousContainer,
-          container,
-          event.previousIndex,
-          event.currentIndex
-        );
-      }
-    }
-
-    event.previousContainer.data.pipe(take(1)).subscribe((data) => {
-      previousContainer = data;
-      updateData();
-    });
-
-    event.container.data.pipe(take(1)).subscribe((data) => {
-      container = data;
-      updateData();
-    });
-  }
-
-  newTask(): void {
-    const dialogRef = this.dialog.open(TaskDialogComponent, {
-      width: '270px',
-      data: {
-        task: {},
-      },
-    });
-    dialogRef
-      .afterClosed()
-      .subscribe((result: TaskDialogResult|undefined) => {
-        if (!result) {
-          return;
-        }
-        this.store.collection('todo').add(result.task);
-      });
-  }
+  constructor() {}
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -24,22 +24,24 @@ import { MatDialogModule } from '@angular/material/dialog';
 import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { TodoScreenComponent } from './todo-screen/todo-screen.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     TaskComponent,
-    TaskDialogComponent
+    TaskDialogComponent,
+    TodoScreenComponent
   ],
   imports: [
     AngularFireModule.initializeApp({
-      apiKey: process.env['apiKey'],
-      authDomain:  process.env['authDomain'],
-      projectId:  process.env['projectId'],
-      storageBucket:  process.env['storageBucket'],
-      messagingSenderId:  process.env['messagingSenderId'],
-      appId:  process.env['appId'],
-      measurementId:  process.env['measurementId'],
+      apiKey: environment.firebaseConfig.apiKey,
+      authDomain:  environment.firebaseConfig.authDomain,
+      projectId:  environment.firebaseConfig.projectId,
+      storageBucket:  environment.firebaseConfig.storageBucket,
+      messagingSenderId:  environment.firebaseConfig.messagingSenderId,
+      appId:  environment.firebaseConfig.appId,
+      measurementId:  environment.firebaseConfig.measurementId,
     }),
     AngularFirestoreModule,
 
